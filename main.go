b950f8b0a658f774cd8b8dc0f31211151a9f005e
rodrigorvsn <rodrigovictor81@gmail.com>
2025-01-28 07:58:35 -0300

    feat: add initial implementation with gin

--- main.go
+++ main.go
@@ -3,22 +3,25 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"net/http"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type User struct {
-	created_at string
-	name       string
-	email      string
-	password   string
-	id         int64
+	CreatedAt string `json:"created_at"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Id        int64  `json:"id"`
 }
 
 // TODO: reimplement the python api in golang and remove python implementation
 
 func main() {
+	engine := gin.Default()
 	db, error := sql.Open("mysql", "root:password@/task_management")
 	if error != nil {
 		panic(error)
@@ -27,19 +30,24 @@ func main() {
 	db.SetMaxOpenConns(10)
 	db.SetMaxIdleConns(10)
 
-	rows, queryError := db.Query("SELECT * FROM Users")
-	if queryError != nil {
-		panic(queryError.Error())
-	}
-
-	var users []User
-
-	for rows.Next() {
-		var user User
-		if error := rows.Scan(&user.id, &user.name, &user.email, &user.created_at, &user.password); error != nil {
-			fmt.Println(error.Error())
+	engine.GET("/users", func(c *gin.Context) {
+		rows, queryError := db.Query("SELECT * FROM Users")
+		if queryError != nil {
+			panic(queryError.Error())
 		}
-		users = append(users, user)
-	}
-	fmt.Println(users)
+
+		var users []User
+
+		for rows.Next() {
+			var user User
+			if error := rows.Scan(&user.Id, &user.Name, &user.Email, &user.CreatedAt, &user.Password); error != nil {
+				fmt.Println(error.Error())
+			}
+			users = append(users, user)
+		}
+		fmt.Println(users)
+		c.JSON(http.StatusOK, users)
+	})
+
+	engine.Run()
 }
