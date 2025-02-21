bbc1b3dc394426a6eff607b2b07636ab1dadb5d9
rodrigorvsn <rodrigovictor81@gmail.com>
2025-02-21 20:59:54 -0300

    feat(wip): create eventbridge rule on task creation

--- src/controller/task/create_task.go
+++ src/controller/task/create_task.go
@@ -1,9 +1,14 @@
 package taskController
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"pam/src/domain/entity"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/eventbridge"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,10 +18,19 @@ func (c *TaskController) CreateTask(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, error.Error())
 		return
 	}
+
 	newId, error := c.taskRepository.CreateTask(task)
 	if error != nil {
 		ctx.JSON(http.StatusBadRequest, error.Error())
 		return
+	}
+
+	cfg, error := config.LoadDefaultConfig(context.TODO(), config.WithDefaultRegion("us-east-1"))
+	client := eventbridge.NewFromConfig(cfg)
+	output, error := client.PutRule(context.TODO(), &eventbridge.PutRuleInput{Name: aws.String(fmt.Sprintf("%d", newId))})
+	fmt.Println(output)
+	if error != nil {
+		ctx.JSON(http.StatusBadRequest, error.Error())
 	}
 	ctx.JSON(http.StatusCreated, newId)
 }
