eca6ea3702a17a15978bc668378b380e606f6b9f
rodrigorvsn <rodrigovictor81@gmail.com>
2025-03-02 10:50:01 -0300

    feat: trigger SNS topic on task creation by eventbridge schedule

--- src/controller/task/create_task.go
+++ src/controller/task/create_task.go
@@ -11,6 +11,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/eventbridge"
+	"github.com/aws/aws-sdk-go-v2/service/eventbridge/types"
 	"github.com/gin-gonic/gin"
 )
 
@@ -34,7 +35,7 @@ func (c *TaskController) CreateTask(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, error.Error())
 	}
 	client := eventbridge.NewFromConfig(cfg)
-	cron := fmt.Sprintf("cron(%d %d %d %d ? %d)", task.DueDate.Minute(), task.DueDate.Hour(), task.DueDate.Day(), task.DueDate.Month(), task.DueDate.Day())
+	cron := fmt.Sprintf("cron(%d %d %d %d ? %d)", task.DueDate.Minute(), task.DueDate.Hour(), task.DueDate.Day(), task.DueDate.Month(), task.DueDate.Year())
 
 	_, err := client.PutRule(
 		context.TODO(),
@@ -44,6 +45,20 @@ func (c *TaskController) CreateTask(ctx *gin.Context) {
 		})
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, error.Error())
+	}
+
+	_, er := client.PutTargets(context.TODO(), &eventbridge.PutTargetsInput{
+		Rule: aws.String(fmt.Sprintf("%d", newId)),
+		Targets: []types.Target{
+			{
+				Id:      aws.String("1"),
+				Arn:     aws.String(os.Getenv("AWS_ARN_SNS_TASK_REMINDER")),
+				RoleArn: aws.String(os.Getenv("AWS_ARN_SNS_TASK_REMINDER_ROLE")),
+			},
+		},
+	})
+	if er != nil {
+		ctx.JSON(http.StatusBadRequest, er.Error())
 	}
 	ctx.JSON(http.StatusCreated, newId)
 }
